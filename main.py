57e2e37c0cc86b1dcf524663cc72590b438dc028
Zach Pedersen <zpedersen@ospreymed.com>
2019-06-11 16:56:42 -0500

    v 0.7 added a gui and improved sales report

--- main.py
+++ main.py
@@ -2,19 +2,55 @@
 
 
 from tkinter import filedialog
+import tkinter as tk
 import clinical_data
 import sales_data
 
 # Main Method of project
 
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+        self.create_widgets()
 
-fileName = filedialog.askopenfilename(initialdir='C:\\', title='Select database file', filetypes=(('sqlite files',
-                                                                                '*.sqlite'), ('all files', '*.*')))
-CMSW = fileName[-23:-20]
-if CMSW[0] == '/':
-    CMSW = CMSW.replace('/', '')
-data_type = input('Enter \"clinical\" to generate a clinical report or \"sales\" to generate a sales report: ')
-if data_type == 'clinical':
-    clinical_data.excel_write(fileName, CMSW)
-elif data_type == 'sales':
-    sales_data.excel_write(fileName, CMSW)
+    def create_widgets(self):
+        self.clinical = tk.Button(self)
+        self.clinical['text'] = 'Generate Clinical Report'
+        self.clinical['command'] = self.clinical_report
+        self.clinical.pack(side='top')
+
+        self.sales = tk.Button(self)
+        self.sales['text'] = 'Generate Sales Report'
+        self.sales['command'] = self.sales_report
+        self.sales.pack(side='right')
+
+        self.quit = tk.Button(self, text='QUIT', fg='red',
+                              command=self.master.destroy)
+        self.quit.pack(side='bottom')
+
+    def clinical_report(self):
+        fileName = filedialog.askopenfilename(initialdir='C:\\', title='Select database file',
+                                                  filetypes=(('sqlite files',
+                                                              '*.sqlite'), ('all files', '*.*')))
+        CMSW = fileName[-23:-20]
+        if CMSW[0] == '/':
+            CMSW = CMSW.replace('/', '')
+        clinical_data.excel_write(fileName, CMSW)
+
+    def sales_report(self):
+        CMSWs = []
+        fileNames = filedialog.askopenfilenames(title='Select database file',
+                                                  filetypes=(('sqlite files','*.sqlite'), ('all files', '*.*')))
+        fileNames = list(fileNames)
+        for file in fileNames:
+            if '/' in file:
+                file = file.replace('/', '')
+            CMSWs.append(file[-23:-20])
+        sales_data.excel_write(fileNames, CMSWs)
+
+
+root = tk.Tk()
+app = Application(master=root)
+app.mainloop()
--- sales_data.py
+++ sales_data.py
@@ -1,12 +1,12 @@
 
 import sqlite3 as sqlite
 import openpyxl
-from openpyxl.styles import Alignment, PatternFill
+from openpyxl.styles import Alignment
 
 # Write data for sales team to appropriate template
 
 
-def excel_write(file_name, cmsw):
+def list_builder(file_name, cmsw):
 
     con = sqlite.connect(file_name)
 
@@ -29,13 +29,42 @@ def excel_write(file_name, cmsw):
             else:
                 color = 0
             cases.append((color, row[5][0:10], row[5][11:22], row[8], row[13], row[15], row[14], row[16]))
-    xlsx_name = cmsw + '-data-tables.xlsx'
+
+def excel_write(file_names, cmsw):
+
+    cases = []
+    for file_name in file_names:
+        con = sqlite.connect(file_name)
+
+        with con:
+
+            cur = con.cursor()
+            cur.execute('SELECT * FROM CMSWCases')
+            rows = cur.fetchall()
+
+            for row in rows:
+                if row[19] <= 5 or row[13] == row[14] == row[15] == row[16] == 0: #
+                    pass
+                else:
+                    if row[15] <= row[8]/3 and row[13] >= row[8]/3:
+                        color = 1
+                    elif row[15] <= row[8]*2/3 and row[13] >= row[8]*2/3:
+                        color = 2
+                    elif row[15] <= row[8] and row[13] >= row[8]:
+                        color = 3
+                    elif row[15] >= row[8] and row[13] >= row[8]:
+                        color = 4
+                    else:
+                        color = 0
+                    cases.append((color, row[5][0:10], row[5][11:22], row[8], row[13], row[15], row[14], row[16]))
+    xlsx_name = str(cmsw) + '-data-tables.xlsx'
     wb = openpyxl.load_workbook('Sales-Template.xlsx')
     data_sheet = wb.active
     data_sheet.title = 'Sheet1'
     for row in range(len(cases)):
         for col in range(len(cases[row])):
-            data_sheet.cell(row=row + 16, column=col + 1, value=cases[row][col])
-            data_sheet.cell(row=row + 16, column=col + 1).alignment = Alignment(wrapText=True)
+            data_sheet.cell(row=row + 17, column=col + 1, value=cases[row][col])
+            data_sheet.cell(row=row + 17, column=col + 1).alignment = Alignment(wrapText=True)
 
+    data_sheet.column_dimensions['A'].hidden = True
     wb.save(xlsx_name)
